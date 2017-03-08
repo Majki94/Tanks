c0624ba32d2b38d93f9965108420e457fed34717
Majki94 <master.atwork@yahoo.com>
2017-03-08 01:44:47 +0100

    Uradjen fling i zoom...

--- core/src/com/tanks/game/MyGdxGame.java
+++ core/src/com/tanks/game/MyGdxGame.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame implements ApplicationListener, GestureDetector.GestureListener {
 
@@ -17,19 +20,34 @@ public class MyGdxGame implements ApplicationListener, GestureDetector.GestureLi
     private OnScreenObject tank;
     private OnScreenObject rocket;
     private float dt;
+    private GestureDetector gd;
+    private float scale;
+    private OrthographicCamera camera;
+    private Viewport vp;
+    private final double ZOOM_SPEED = 0.004;
 
     @Override
     public void create() {
         mainStage = new GameStage();
+
         tank = ((GameStage) mainStage).getTank();
         rocket = ((GameStage) mainStage).getRocket();
+
+        gd = new GestureDetector(this);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        vp = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+
+        mainStage.setViewport(vp);
+
+        scale = (float) (Gdx.graphics.getWidth() * 0.000004167);
     }
 
     @Override
     public void render() {
 
         //nova raketa
-        if(rocket.getY() < 0){
+        if (rocket.getY() < 0) {
             rocket.setVisible(false);
             rocket.setPosition(tank.getX() + tank.getWidth(), tank.getY() + tank.getHeight());
             rocket.started = false;
@@ -44,7 +62,6 @@ public class MyGdxGame implements ApplicationListener, GestureDetector.GestureLi
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        GestureDetector gd = new GestureDetector(this);
 
         Gdx.input.setInputProcessor(gd);
 
@@ -71,13 +88,14 @@ public class MyGdxGame implements ApplicationListener, GestureDetector.GestureLi
     public boolean fling(float velocityX, float velocityY, int button) {
         rocket.setVisible(true);
         rocket.started = true;
-        rocket.brzinaX = velocityX * 30;
-        rocket.brzinaY = velocityY * 30;
-        System.out.print("V x = " + velocityX + "   ");
-        System.out.println("V y = " + velocityY);
-        System.out.print("Brzina X = " + rocket.brzinaX + "   ");
-        System.out.println("Brzina Y = " + rocket.brzinaY);
-        return true;
+        rocket.brzinaX = velocityX * scale;
+        rocket.brzinaY = -velocityY * scale;
+//        System.out.print("V x = " + velocityX + "   ");
+//        System.out.println("V y = " + velocityY);
+//        System.out.print("Brzina X = " + rocket.brzinaX + "   ");
+//        System.out.println("Brzina Y = " + rocket.brzinaY);
+        System.out.println("FLING!!!!!!");
+        return false;
     }
 
     @Override
@@ -99,6 +117,7 @@ public class MyGdxGame implements ApplicationListener, GestureDetector.GestureLi
     public boolean pan(float x, float y, float deltaX, float deltaY) {
 //        tank.setPosition(x, Gdx.graphics.getHeight() - y);
 //        if (mainStage instanceof GameStage){
+
 //            ((GameStage) mainStage).update();
 //        }
         return false;
@@ -111,7 +130,14 @@ public class MyGdxGame implements ApplicationListener, GestureDetector.GestureLi
 
     @Override
     public boolean zoom(float initialDistance, float distance) {
+        if (initialDistance < distance) {
+            camera.zoom *= (1 - ZOOM_SPEED);
+        } else {
+            camera.zoom *= (1 + ZOOM_SPEED);
+        }
+        System.out.println("ZOOOOOOM!!!!!!");
         return false;
+
     }
 
     @Override
